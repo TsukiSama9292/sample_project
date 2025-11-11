f3fdb115828490a17213495a753db4399caec6d5
TsukiSama9292 <a0985821880@gmail.com>
2025-11-11 20:29:54 +0800

    [update] 單元

--- projectdev/ai/urls.py
+++ projectdev/ai/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/trigger/', views.trigger, name='trigger'),
+    path('api/another/', views.another_view, name='another_view'),
 ]
